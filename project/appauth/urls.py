bcd73b7c74f127afb5ff5315de724f168f5d7175
shital <chavanshital3108@gmail.com>
2025-07-29 18:02:41 +0530

    register changes to signup

--- project/appauth/urls.py
+++ project/appauth/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import SignAPIView,OTPVerificationAPIview,SigninAPIView
 urlpatterns =[
-    path('register/',SignAPIView.as_view(),name='signup'),
+    path('signup/',SignAPIView.as_view(),name='signup'),
     path('verify-otp/',OTPVerificationAPIview.as_view(),name="verify_otp"),
     path('signin/',SigninAPIView.as_view(),name='signin'),
 ]
